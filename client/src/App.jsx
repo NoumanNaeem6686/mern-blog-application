096b61e128bfd4f943be4833a5bf780b555200c9
NoumanNaeem6686 <03035777248a@gmail.com>
2024-03-23 16:00:33 +0500

    create responsive header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,10 +5,12 @@ import Signin from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
